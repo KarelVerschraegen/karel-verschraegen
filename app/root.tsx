d5f2ec910ed8bc21ef54d832e61b8cbc7049cf50
Karel Verschraegen <karel.verschraegen@hotmail.com>
2022-09-03 17:16:49 +0200

    Update page title

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,7 @@ import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
 	charset: "utf-8",
-	title: "New Remix App",
+	title: "Karel Verschraegen",
 	viewport: "width=device-width,initial-scale=1",
 });
 
