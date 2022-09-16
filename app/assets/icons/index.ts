aefb6745295c2d6697a0ac101dfe0ee53aaa02e5
Karel Verschraegen <karel.verschraegen@hotmail.com>
2022-09-16 23:37:22 +0200

    Add social SVG icons to assets folder

---
+++ app/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { Email } from "./Email";
+export { GitHub } from "./GitHub";
+export { Instagram } from "./Instagram";
+export { LinkedIn } from "./LinkedIn";
+export { Twitter } from "./Twitter";
