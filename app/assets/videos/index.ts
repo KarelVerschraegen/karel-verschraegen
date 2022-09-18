d14153a46ace7676ec36f1df354f9730c1cbbc8a
Karel Verschraegen <karel.verschraegen@hotmail.com>
2022-09-18 22:45:14 +0200

    Write out projects section

---
+++ app/assets/videos/index.ts
@@ -0,0 +1,6 @@
+import EquipmentList from "./equipment-list.mp4";
+import EquipmentListFirstFrame from "./equipment-list-first-frame.jpg";
+import LiquidEditor from "./liquid-editor.mp4";
+import LiquidEditorFirstFrame from "./liquid-editor-first-frame.jpg";
+
+export { EquipmentList, EquipmentListFirstFrame, LiquidEditor, LiquidEditorFirstFrame };
