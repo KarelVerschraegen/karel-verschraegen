e6aa6489e77838ade7406d270adc2a70af161f2c
Karel Verschraegen <karel.verschraegen@hotmail.com>
2022-09-18 23:43:37 +0200

    Compress images and convert to webp format

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,5 +1,5 @@
-import Karel from "./karel.jpg";
-import KarelFunny from "./karel-funny.jpg";
-import Wisserke from "./wisserke.png";
+import Karel from "./karel.webp";
+import KarelFunny from "./karel-funny.webp";
+import Wisserke from "./wisserke.webp";
 
 export { Karel, KarelFunny, Wisserke };
--- app/assets/videos/index.ts
+++ app/assets/videos/index.ts
@@ -1,6 +1,6 @@
 import EquipmentList from "./equipment-list.mp4";
-import EquipmentListFirstFrame from "./equipment-list-first-frame.jpg";
+import EquipmentListFirstFrame from "./equipment-list-first-frame.webp";
 import LiquidEditor from "./liquid-editor.mp4";
-import LiquidEditorFirstFrame from "./liquid-editor-first-frame.jpg";
+import LiquidEditorFirstFrame from "./liquid-editor-first-frame.webp";
 
 export { EquipmentList, EquipmentListFirstFrame, LiquidEditor, LiquidEditorFirstFrame };
