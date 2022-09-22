71510216b6ac5b66c45596809056bfa6fb809445
Karel Verschraegen <karel.verschraegen@hotmail.com>
2022-09-22 17:35:22 +0200

    Add web-vitals analytics tracking

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,13 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
 
+import { webVitals } from "./lib/vitals";
+
 hydrateRoot(document, <RemixBrowser />);
+
+if (process.env.VERCEL_ANALYTICS_ID) {
+	webVitals({
+		analyticsId: process.env.VERCEL_ANALYTICS_ID as string,
+		path: "/",
+	});
+}
